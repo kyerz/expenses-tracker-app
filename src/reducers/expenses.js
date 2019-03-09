36beadf158c6a0a9d9472e0f7509a0a4b6bec7da
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 21:11:46 +0100

    add redux package & create expenses reducer

---
+++ src/reducers/expenses.js
@@ -0,0 +1,12 @@
+//reducer expenses
+
+const initStateExpensesReducer = []
+
+const expensesReducer = (state = initStateExpensesReducer, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default expensesReducer
