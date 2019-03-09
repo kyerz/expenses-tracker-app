eb6d4ea0798c07eac26ebe27a4960d6fea40baec
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 19:21:53 +0100

    start routing

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import Header from '../components/Header'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import ExpenseDashBoardPage from '../components/ExpenseDashboardPage'
 import AddExpensePage from '../components/AddExpensePage'
 import NotFoundPage from '../components/NotFoundPage'
 
 
 const AppRouter = () => (
-  <div>
-    <Header />
-  </div>
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={ExpenseDashBoardPage} />
+        <Route path='/create' component={AddExpensePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </BrowserRouter>
 )
 
 export default AppRouter
