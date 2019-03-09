d8c91b6fefb37caae9bd7e6e410423b2d4959e9b
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 19:13:44 +0100

    create ExpenseDashboardPage component

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpenseDashboardPage = () => (
+  <div>
+    ExpenseDashboardPage component
+  </div>
+)
+
+export default ExpenseDashboardPage
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Header from '../components/Header'
 
 const AppRouter = () => (
-  <div>AppRouter</div>
+  <div>
+    <Header />
+  </div>
 )
 
 export default AppRouter
