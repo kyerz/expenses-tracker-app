712db1642bd9a36333b08b0bf08e5cdc33d48dc1
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 19:24:16 +0100

    add Navlink to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => (
   <div>
     <h1>Expenses Tracker</h1>
+    <NavLink to="/">DashBoard</NavLink>
+    <NavLink to="/create">Create Expense</NavLink>
   </div>
 )
 
