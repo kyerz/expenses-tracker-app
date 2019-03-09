ac182b5797714e6f8685c6f4693af5e9170ca854
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 19:15:39 +0100

    create AddExpensePage component

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddExpensePage = () => (
+  <div>
+    AddExpensePage component
+  </div>
+)
+
+export default AddExpensePage
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Header from '../components/Header'
+import ExpenseDashBoardPage from '../components/ExpenseDashboardPage'
+import AddExpensePage from '../components/AddExpensePage'
+
 
 const AppRouter = () => (
   <div>
