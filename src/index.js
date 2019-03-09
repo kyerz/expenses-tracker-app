b3c4c3ccfabf992e7f7bd05c568c464f1475398f
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 19:27:51 +0100

    improve Navlink

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
+import './styles/base/style.css'
 
 ReactDOM.render(<div><AppRouter /></div>, document.querySelector('#root'))
