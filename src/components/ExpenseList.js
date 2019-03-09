febda3a8f07af0957c936aa25a2e127b621b4f5d
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-09 22:32:12 +0100

    create ExpenseListItem component

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import ExpenseListItem from './ExpenseListItem'
 
 const ExpenseList = () => (
   <div>
     <h2>Expense List Component</h2>
+    <ExpenseListItem />
   </div>
 )
 
---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpenseListItem = () => (
+  <div>
+    Expense List item
+  </div>
+)
+
+export default ExpenseListItem
