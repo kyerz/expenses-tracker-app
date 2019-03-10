a3d8c56185849baf066e88616c2eb7dc35797bab
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 02:15:39 +0100

    change ExpenseForm into smart component(class)

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,25 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ExpenseForm = () => (
-  <div>
-    <form>
-      <input
-        type="text"
-        placeholder="description"
-      />
-      <input
-        type="number"
-        placeholder="amount"
-      />
-      <textarea
-        placeholder="you can write a note here"
-        cols="30"
-        rows="2"
-      >
-      </textarea>
-      <button>Add Expense</button>
-    </form>
-  </div>
-)
+class ExpenseForm extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            placeholder="description"
+          />
+          <input
+            type="number"
+            placeholder="amount"
+          />
+          <textarea
+            placeholder="you can write a note here"
+            cols="30"
+            rows="2"
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default ExpenseForm
