b93632767ae733e62495543f7cf0b2f6e14b5204
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 14:49:37 +0100

    fix deployment

---
+++ build/precache-manifest.b546841d4afd384148d33be0b4f4ce0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0593109fb52e3bc4c83c",
+    "url": "/static/js/main.fba7017e.chunk.js"
+  },
+  {
+    "revision": "e8c8b264caa3ff222a46",
+    "url": "/static/js/2.6babed91.chunk.js"
+  },
+  {
+    "revision": "0593109fb52e3bc4c83c",
+    "url": "/static/css/main.0f9d34df.chunk.css"
+  },
+  {
+    "revision": "e8c8b264caa3ff222a46",
+    "url": "/static/css/2.85a5503b.chunk.css"
+  },
+  {
+    "revision": "cdac757c5362fb75dafa05d1053740d9",
+    "url": "/index.html"
+  }
+];
