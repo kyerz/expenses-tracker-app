ba5ef955ca09a67621efeded75f83b6ea37813d9
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 02:07:03 +0100

    create ExpenseForm component

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import ExpenseForm from './ExpenseForm'
 const AddExpensePage = () => (
   <div>
     <h2>Add New Expense</h2>
+    <ExpenseForm />
   </div>
 )
 
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExpenseForm = () => (
+  <div>
+    ExpenseForm component
+  </div>
+)
+
+export default ExpenseForm
