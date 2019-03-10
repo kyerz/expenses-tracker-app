88986064a8ed3239ecd28e326fcbda311c7dad9d
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 02:02:54 +0100

    input filter sort by amount or date done

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { setSearchText } from '../actions/filters'
-
+import { sortByDate, sortByAmount } from '../actions/filters'
 
 const ExpenseListFilters = (props) => (
   <div>
@@ -11,7 +11,20 @@ const ExpenseListFilters = (props) => (
       value={props.filters.text}
       onChange={(e) => props.dispatch(setSearchText(e.target.value))}
     />
-  </div>
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount') {
+          props.dispatch(sortByAmount())
+        }
+      }}
+    >
+      <option value="date">date</option>
+      <option value="amount">amount</option>
+    </select>
+  </div >
 )
 
 const mapStateToProps = (state) => {
