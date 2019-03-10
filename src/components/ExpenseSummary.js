b8d48ea4f38e96f3969c9dab07f265b29b1f1470
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 22:58:57 +0100

    fix ignored files

---
+++ src/components/ExpenseSummary.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import numeral from 'numeral'
+import visibleExpensesFilter from '../filters/expenses'
+import totalExpenses from '../filters/total-expenses'
+
+const ExpenseSummary = ({ expensesCount, expensesTotal }) => {
+  const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'
+  const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
+  return (
+    <div>
+      <h1>Viewing {expensesCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+
+  const filteredExpenses = visibleExpensesFilter(state.expenses, state.filters)
+
+  return {
+    expensesTotal: totalExpenses(filteredExpenses),
+    expensesCount: filteredExpenses.length
+  }
+}
+
+export default connect(mapStateToProps)(ExpenseSummary)
