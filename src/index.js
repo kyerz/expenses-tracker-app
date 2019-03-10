885a1128df06b46b8bfaf25a3e50f8bfd176a369
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 17:29:14 +0100

    clean dummy dispatch

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux'
 import './styles/base/style.css'
 import initStore from './store/initStore'
-import { addExpense } from './actions/expenses'
 import visibleExpensesFilter from './filters/expenses'
 
 const store = initStore()
@@ -13,13 +12,6 @@ store.subscribe(() => {
   const state = store.getState()
   console.log(visibleExpensesFilter(state.expenses, state.filters))
 })
-
-
-//dummy dispatch
-store.dispatch(addExpense({ description: 'facture d\'électricité', amount: 13000, createdAt: 1552183636916 }))
-store.dispatch(addExpense({ description: 'Facture d\'eau', amount: 6500, createdAt: 1552184837125 }))
-store.dispatch(addExpense({ description: 'Location voiture', amount: 8500, createdAt: 1567928619734 }))
-
 
 const jsx = (
   <Provider store={store}>
