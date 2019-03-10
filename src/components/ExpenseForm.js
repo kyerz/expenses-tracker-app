4b243e9539acbd530331713104b57b544923ec19
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-10 02:13:07 +0100

    start building form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 const ExpenseForm = () => (
   <div>
-    ExpenseForm component
+    <form>
+      <input
+        type="text"
+        placeholder="description"
+      />
+      <input
+        type="number"
+        placeholder="amount"
+      />
+      <textarea
+        placeholder="you can write a note here"
+        cols="30"
+        rows="2"
+      >
+      </textarea>
+      <button>Add Expense</button>
+    </form>
   </div>
 )
 
