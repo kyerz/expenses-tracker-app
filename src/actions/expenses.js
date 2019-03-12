49c81b142baa76054bca2e5b2fd8de54fcefc0d8
Mikael Verdu <mikael.verdu@gmail.com>
2019-03-12 19:13:54 +0100

    implement action to fetch data from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,9 +32,37 @@ export const removeExpense = (id) => ({
   id
 })
 
+
 //edit expense
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
 })
+
+
+//set expenses
+
+
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+})
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value')
+      .then(snapshot => {
+        const expenses = []
+
+        snapshot.forEach(childSnapshot => {
+          expenses.push({
+            id: childSnapshot.key,
+            ...childSnapshot.val()
+          })
+        })
+        dispatch(setExpenses(expenses))
+      })
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -4,20 +4,22 @@ import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux'
 import './styles/base/style.css'
 import initStore from './store/initStore'
-import visibleExpensesFilter from './filters/expenses'
+import { startSetExpenses } from './actions/expenses'
 import './firebase/firebase'
 
 const store = initStore()
 
-store.subscribe(() => {
-  const state = store.getState()
-  console.log(visibleExpensesFilter(state.expenses, state.filters))
-})
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
 )
+ReactDOM.render(<p>Loading...</p>, document.querySelector('#root'))
 
-ReactDOM.render(jsx, document.querySelector('#root'))
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx, document.querySelector('#root'))
+})
+
+
